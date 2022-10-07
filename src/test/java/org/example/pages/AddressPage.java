76d414b793c1b1722ff7432d0fd042662cc7d025
esraayildiz <100995928+esraayildiz@users.noreply.github.com>
2022-10-07 10:32:44 +0300

    Delete AddressPage.java

--- src/test/java/org/example/pages/AddressPage.java
+++
@@ -1,8 +0,0 @@
-package org.example.pages;
-
-public class AddressPage extends BasePage{
-
-    public void fillAddressInformation(){
-        //methods.sendKeys(AddressPageConstants7.nameTextBox);
-    }
-}
